fe31376566ebff89a7fc64562b68a6bc4ce61166
ItsRajSingh <raj.singh.physica@gmail.com>
2024-07-27 14:42:58 +0530

    Initial dart learning

---
+++ Operators.dart
@@ -0,0 +1,17 @@
+void main(){
+  int num1 = 10;
+  int num2 = 3;
+  int sum = num1 + num2;
+  int diff =  num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2 ; 
+  int div2 = num1~/num2;
+  int unaryMinus = -num1;
+  print( "The sum is $sum");
+  print( "The difference is $diff");
+  print("The mul is $mul");
+  print("The div is $div ");
+  print("The unary minus is $unaryMinus");
+  print("The integer division is $div2");
+  
+}
