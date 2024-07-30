8fdd4c9158b9e70a044713b1fa090ac43e854832
ItsRajSingh <raj.singh.physica@gmail.com>
2024-07-30 12:33:18 +0530

    Ternary Operator usage

---
+++ Ternary.dart
@@ -0,0 +1,15 @@
+void main()
+{
+  int num1 =10;
+  int num2 = 15;
+  int max = (num1>num2)?num1:num2;
+  print("The greatest number is $max");
+  //Example 2
+  var selection = 2;
+  var output = (selection == 2)? "Apple": "Banana";
+  print(output);
+  //Example 3
+  var age =21;
+  var check = (age >=13 && age <=19)?"You are a teen": "You are an adult";
+  print(check);
+}  
